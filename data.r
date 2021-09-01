b5b41643c56ca3520b881886f2cd43e9c794901e
Gabriel <catano@uthscsa.edu>
2021-09-01 16:34:38 -0500

    Added simulated data

--- data.r
+++ data.r
@@ -1,5 +1,5 @@
 #+ init, echo=FALSE, message=FALSE, warning=FALSE,results='hide'
-
+inputdata <- "C:/Users/GABE/Desktop/R/hcvdat0.csv"
 debug <- 0;
 knitr::opts_chunk$set(echo=debug>0, warning=debug>0, message=debug>0);
 #' Load libraries
@@ -11,13 +11,13 @@ library(synthpop);
 #' Here are the libraries R currently sees:
 search() %>% pander();
 #' Load data
-# dat0 <- import('example_file.xlsx');
-dat0 <- survival::veteran;
+dat0 <- import(inputdata);
+dat0cb <- codebook.syn(dat0)
 #' Make a scatterplot matrix
-ggpairs(dat0);
+#' ggpairs(dat0);
 #' Set all the two-value columns to be TRUE/FALSE
 dat1 <- mutate(dat0
-               , across(where( function(xx) length(unique(xx))==2), as.logical));
+               , across(where( function(xx) length(unique(xx))==2), factor));
 #' Now try the scatterplot matrix again
-ggpairs(dat1);
+#' ggpairs(dat1);
 
