4216e97027ab8bd6bcb1f66084486fe0e179a376
Gabriel <catano@uthscsa.edu>
2021-10-06 16:59:51 -0500

    Intro to Python 1

--- Intro_to_python.py
+++ Intro_to_python.py
@@ -1,5 +1,6 @@
 #Intro to python on October 6, 2021
 import pandas as pd  #load a python library
+import matplotlib.pyplot as plt
 dat0 = pd.read_csv("data/sim_data.csv")
 dat0
 dat0.info()  
@@ -21,3 +22,16 @@ dat0.iloc[10:30,2:5]
 dat0['Ag e'] = 42
 dat0.head()
 dat0.eval('Age+PROT')
+dat0.columns
+dat0.columns[1]
+dat0.iloc[1]
+dat0.iloc[:, 1]
+
+AP1=dat0.plot.scatter('ALB', 'PROT')
+AP1=dat0.plot.scatter('ALB', 'PROT', c='Green')
+AP1=dat0[dat0.Sex=='f'].plot.scatter('ALB', 'PROT', c='Green')
+AP2=dat0[dat0.Sex=='m'].plot.scatter('ALB', 'PROT', c='Red')
+AP1=dat0[dat0.Sex=='f'].plot.scatter('ALB', 'PROT', c='Green', label='f')
+AP2=dat0[dat0.Sex=='m'].plot.scatter('ALB', 'PROT', c='Red', label='m',ax=AP1)
+plt.show()
+
---
+++ example.r
@@ -0,0 +1,5 @@
+py_install('pandas') # install modules
+
+import('pandas') # library command in r
+
+repl_python()  # enter py
